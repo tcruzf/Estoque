4ac2a4cb7eceef48e10e721c04afd4365550bb55
tcruzf <thiago@thiagoferreira.net>
2025-03-06 17:13:25 -0300

    Nova tela de login

--- ControllRR.Presentation/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ControllRR.Presentation/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -66,15 +66,18 @@ namespace ControllRR.Presentation.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+           
             [EmailAddress]
+            [Display(Name = "Email")]
+            [Required(ErrorMessage = "{0} é obrigatorio!")]
             public string Email { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+            [Display(Name = "Senha")]
+            [Required(ErrorMessage = "{0} é obrigatorio!")]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
